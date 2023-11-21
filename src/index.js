99d611f495c3e1c001a3474a8409d71fa7a6cefd
fmirurr <fcomirandau@gmail.com>
2023-11-21 12:28:45 -0300

    example

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./env";
+import * as userService from "./services/users";
+import { connectAllDb } from "./connectionManager";
+import * as connectionResolver from "./middlewares/connectionResolver";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 8080;
+
+const app = express();
+
+app.set("port", PORT);
+app.use(bodyParser.json());
+
+connectAllDb();
+
+app.use(connectionResolver.resolve);
+
+// API Route
+app.get("/", (req, res, next) => {
+  res.json({ body: "Hello multi-tenant application." });
+});
+
+app.get("/users", async (req, res, next) => {
+  const users = await userService.getAll();
+
+  res.json({ body: users });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server started at port: ${PORT}`);
+});
